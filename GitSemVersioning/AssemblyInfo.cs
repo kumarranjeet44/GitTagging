b2f570b72335dfad62df819f664f5773d2f7bce0
unknown <IE4LLTD9XZCY3$@global.ds.honeywell.com>
2025-07-04 23:39:09 +0530

    Update AssemblyInfo.cs version [CI skip]

--- GitSemVersioning/AssemblyInfo.cs
+++ GitSemVersioning/AssemblyInfo.cs
@@ -31,8 +31,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.2.0.10")]
-[assembly: AssemblyVersion("0.2.0.10")]
-[assembly: AssemblyFileVersion("0.2.0.10")]
+// [assembly: AssemblyVersion("0.2.0.12")]
+[assembly: AssemblyVersion("0.2.0.12")]
+[assembly: AssemblyFileVersion("0.2.0.12")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Honeywell.UOP.TrayDesign.Win229.TestHarness")]
