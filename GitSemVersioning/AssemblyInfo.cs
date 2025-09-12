853cf8ecbee214f51b90664f5102ae5b95dd2920
Yadav, Ranjeet <Ranjeet.Yadav@Honeywell.com>
2025-09-12 20:53:28 +0530

    updating assembly file

--- GitSemVersioning/AssemblyInfo.cs
+++ GitSemVersioning/AssemblyInfo.cs
@@ -1,16 +1,18 @@
 using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Windows;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UOP Win229")]
+[assembly: AssemblyTitle("Honeywell.UOP.MSD")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Honeywell")]
-[assembly: AssemblyProduct("UOP Win229")]
-[assembly: AssemblyCopyright("Copyright © Honeywell 2009")]
+[assembly: AssemblyProduct("Honeywell.UOP.MSD")]
+[assembly: AssemblyCopyright("Copyright © Honeywell 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,8 +21,25 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("39a08069-1dc6-400e-a9c7-7c4e96813d0c")]
+//In order to begin building localizable applications, set 
+//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
+//inside a <PropertyGroup>.  For example, if you are using US english
+//in your source files, set the <UICulture> to en-US.  Then uncomment
+//the NeutralResourceLanguage attribute below.  Update the "en-US" in
+//the line below to match the UICulture setting in the project file.
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+    //(used if a resource is not found in the page, 
+    // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+    //(used if a resource is not found in the page, 
+    // app, or any theme specific resource dictionaries)
+)]
+
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,8 +50,11 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("4.5.0.2")]
 
-// [assembly: AssemblyVersion("2.3.0.2")]
-[assembly: AssemblyVersion("2.3.0.2")]
-[assembly: AssemblyFileVersion("2.3.0.2")]
-[assembly: AssemblyInformationalVersion("4.3.0-feature.58")]
+// Below parameter will get updated and be in proper sync with MSI version and Git Tag only if we run UpdateAssemblyBackToOrigin Task in build.cake file
+// It currently without running UpdateAssemblyBackToOrigin Task, GitTagversion, Github App version and MSI version will be in proper sync
+[assembly: AssemblyVersion("8.2.0.20")]
+[assembly: AssemblyFileVersion("8.2.0.20")]
+[assembly: AssemblyInformationalVersion("8.2.0-alpha.20")]
+
